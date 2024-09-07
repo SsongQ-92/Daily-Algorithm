2dc77c01514036e3d4cf8bf91a85251d4d4ceb82
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-09-07 14:37:21 +0900

    240907_백준_1008_A/B

---
+++ 2409/240907/baekjoon_1008.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './text.txt';
+const input = fs.readFileSync(file).toString().trim().split(" ");
+
+const A = input[0];
+const B = input[1];
+
+const answer = Number((A / B).toFixed(9));
+
+console.log(answer);
