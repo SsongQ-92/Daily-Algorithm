3d859085478d1745a0b790d38ca408a42d7edca0
Ssong-Q <skdmlrh12@gmail.com>
2024-09-14 13:55:22 +0900

    240914_add teacherSolution

--- 2409/240914/재귀함수를이용한이진수출력.js
+++ 2409/240914/재귀함수를이용한이진수출력.js
@@ -18,3 +18,16 @@ function solution(n) {
 }
 
 console.log(solution(11))
+
+function teacherSolution(n){
+  let answer="";
+  function DFS(n){
+       if(n===0) return;
+       else{
+           DFS(parseInt(n/2));
+           answer+=String(n%2);
+       }
+  }
+  DFS(n);
+  return answer;
+}
