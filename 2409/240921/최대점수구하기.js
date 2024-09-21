da96c6d334f0566e78304f6429aa500ffeee786f
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-09-21 16:09:12 +0900

    240921_최대점수구하기_이진트리 dfs; 강사 풀이 추가

--- 2409/240921/최대점수구하기.js
+++ 2409/240921/최대점수구하기.js
@@ -38,3 +38,25 @@ function solution(array) {
 }
 
 console.log(solution([[5, 20], [10, 5], [25, 12], [15, 8], [6, 3], [7, 4]]));
+
+function teacherSolution(m, ps, pt) {         
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = ps.length;
+  function DFS(L, sum, time){
+      if (time > m) return;
+      if (L === n) {
+        answer = Math.max(answer, sum);
+      } else{
+        DFS(L + 1, sum + ps[L], time + pt[L]);
+        DFS(L + 1, sum, time);
+      }   
+  }
+
+  DFS(0, 0, 0);
+  return answer;
+}
+
+let ps = [10, 25, 15, 6, 7];
+let pt = [5, 12, 8, 3, 4];
+
+console.log(teacherSolution(20, ps, pt));
