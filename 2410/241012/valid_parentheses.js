a08cef8328297fac47685c8d0859e333fe3321d0
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-10-12 15:28:22 +0900

    241012_leetcode_valid-parentheses

---
+++ 2410/241012/valid_parentheses.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function(s) {
+  let answer = true;
+  const stack = [];
+  
+  function checkStack(stackLetter, pickUpLetter) {
+      let result = false;
+      
+      switch (stackLetter) {
+          case "(":
+              if (pickUpLetter === ")") {
+                  result = true;
+              }
+              break;
+          case "{":
+              if (pickUpLetter === "}") {
+                  result = true;
+              }
+              break;
+          case "[":
+              if (pickUpLetter === "]") {
+                  result = true;
+              }
+              break;
+          default:
+              break;
+      }
+      
+      return result;
+  }
+  
+  for (let i = 0; i < s.length; i++) {
+      const pickUpLetter = s[i];  
+      const lastLetter = stack[stack.length - 1];
+      
+      if (stack.length !== 0 && checkStack(lastLetter, pickUpLetter)) {
+          stack.pop();
+          
+          continue;
+      } 
+          
+      stack.push(pickUpLetter);
+  }
+  
+  if (stack.length !== 0) {
+      answer = false;
+  }
+  
+  return answer;
+};
