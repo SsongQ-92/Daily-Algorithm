4a4575541cdccd71df08478e0ff1e01aa3a20af9
Ssong-Q <skdmlrh12@gmail.com>
2024-10-20 11:12:20 +0900

    leetcode_bestTimeToBuyAndSellStock

---
+++ 2410/241020/bestTimeToBuyAndSellStock.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices) {
+  if (prices.length === 1) return 0;
+  
+  let answer = 0;
+  let boughtStock = prices[0];
+  
+  for (let i = 1; i < prices.length; i++) {
+      const stock = prices[i];
+      
+      if (stock < boughtStock) {
+          boughtStock = stock;
+          continue;
+      } else {
+          const diff = stock - boughtStock;
+          
+          answer = Math.max(answer, diff);
+      }
+  }
+      
+  return answer;
+};
