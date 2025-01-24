bd822dffff899daadebb37a0f3e5ce35d43e5460
Ssong-Q <skdmlrh12@gmail.com>
2025-01-24 22:52:32 +0900

    Leet code: Balanced Binary Tree

---
+++ 2501/250124/balancedBinaryTree.ts
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/balanced-binary-tree/description/
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isBalanced(root: TreeNode | null): boolean {
+  const checkHeight = (node: TreeNode | null): number => {
+    if (node === null) return 0;
+
+    const leftHeight = checkHeight(node.left);
+    if (leftHeight === -1) return -1;
+
+    const rightHeight = checkHeight(node.right);
+    if (rightHeight === -1) return -1;
+
+    if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  };
+
+  return checkHeight(root) !== -1;
+};
