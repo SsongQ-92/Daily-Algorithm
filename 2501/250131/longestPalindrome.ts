ec066c4c020d1699ad0e82f146233ef959e0ddd4
Ssong-Q <skdmlrh12@gmail.com>
2025-02-01 14:48:25 +0900

    Leet code: Longest Palindrome

---
+++ 2501/250131/longestPalindrome.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-palindrome/
+
+function longestPalindrome(s: string): number {
+  const freq = new Map<string, number>();
+
+  for (const char of s) {
+    freq.set(char, (freq.get(char) ?? 0) + 1);
+  }
+
+  let length = 0;
+  let hasOdd = false;
+
+  for (const count of freq.values()) {
+    if (count % 2 === 0) {
+      length += count;
+    } else {
+      length += count - 1;
+      hasOdd = true;
+    }
+  }
+
+  return hasOdd ? length + 1 : length;
+};
