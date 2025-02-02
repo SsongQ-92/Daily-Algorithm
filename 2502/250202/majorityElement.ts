ac3661f625de2479e3bbe2184c9482a1716b3414
Ssong-Q <skdmlrh12@gmail.com>
2025-02-02 16:31:35 +0900

    Leet code: Majority Element

---
+++ 2502/250202/majorityElement.ts
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/majority-element/description/
+
+function majorityElement(nums: number[]): number {
+  let candidate: number = nums[0];
+  let count: number = 0;
+
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    
+    count += (num === candidate) ? 1 : -1;
+  }
+
+  return candidate;
+};
