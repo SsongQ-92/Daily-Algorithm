b47050eaf54beaa2cc304226d19698dda4db37e6
Ssong-Q <skdmlrh12@gmail.com>
2025-02-05 22:48:22 +0900

    Leet code: Contains Duplicate

---
+++ 2502/250205/containsDuplicate.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+function containsDuplicate(nums: number[]): boolean {
+  const seen = new Set<number>();
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return true;
+    }
+    
+    seen.add(num);
+  }
+  
+  return false;
+};
