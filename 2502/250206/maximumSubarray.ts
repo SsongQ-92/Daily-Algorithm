413a3365f432f4c97ea5664a7219aa0e4c78babe
Ssong-Q <skdmlrh12@gmail.com>
2025-02-06 20:17:30 +0900

    Leet code: Maximum Subarray

---
+++ 2502/250206/maximumSubarray.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/maximum-subarray/description/
+
+function maxSubArray(nums: number[]): number {
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+  
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    
+    maxSum = Math.max(maxSum, currentSum);
+  }
+  
+  return maxSum;
+};
