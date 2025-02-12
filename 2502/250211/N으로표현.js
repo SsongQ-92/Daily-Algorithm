0252dc615376533ae72db6ace900bca1567d723c
Kyukyoung Song <skdmlrh12@gmail.com>
2025-02-12 17:00:09 +0900

    Programmers: N으로 표현

---
+++ 2502/250211/N으로표현.js
@@ -0,0 +1,27 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42895
+
+function solution(N, number) {
+  if (N === number) return 1;
+
+  const dp = Array.from({ length: 9 }, () => new Set());
+
+  for (let i = 1; i <= 8; i++) {
+    dp[i].add(Number(N.toString().repeat(i))); 
+    
+    for (let j = 1; j < i; j++) {
+      for (let num1 of dp[j]) {
+        for (let num2 of dp[i - j]) {
+          dp[i].add(num1 + num2);
+          dp[i].add(num1 - num2);
+          dp[i].add(num1 * num2);
+          
+          if (num2 !== 0) dp[i].add(Math.floor(num1 / num2));
+        }
+      }
+    }
+    
+    if (dp[i].has(number)) return i;
+  }
+
+  return -1;
+}
