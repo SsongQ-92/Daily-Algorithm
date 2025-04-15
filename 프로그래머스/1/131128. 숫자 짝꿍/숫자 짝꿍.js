675add55a3abc6a5d0b3d3eb1b28de2d3caf6d3b
Kyukyoung Song <ssongq92@gmail.com>
2025-04-15 21:08:44 +0900

    [level 1] Title: 숫자 짝꿍, Time: 223.49 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,17 @@
+function solution(X, Y) {
+  const countX = Array(10).fill(0);
+  const countY = Array(10).fill(0);
+
+  for (let digit of X) countX[digit]++;
+  for (let digit of Y) countY[digit]++;
+
+  let result = '';
+  for (let i = 9; i >= 0; i--) {
+    const minCount = Math.min(countX[i], countY[i]);
+    result += i.toString().repeat(minCount);
+  }
+
+  if (!result) return '-1';
+  if (result[0] === '0') return '0';
+  return result;
+}
